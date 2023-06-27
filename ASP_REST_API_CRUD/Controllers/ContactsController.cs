0fa1467947afdebed08a3379a4e35ebdbf121675
anhhuydepzai <anhhuydepzai@gmail.com>
2023-06-27 12:33:15 +0700

    add a new contact

--- ASP_REST_API_CRUD/Controllers/ContactsController.cs
+++ ASP_REST_API_CRUD/Controllers/ContactsController.cs
@@ -1,5 +1,7 @@
 ﻿using ASP_REST_API_CRUD.Data;
+using ASP_REST_API_CRUD.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASP_REST_API_CRUD.Controllers
 {
@@ -15,9 +17,29 @@ namespace ASP_REST_API_CRUD.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetContacts()
+        //Get contact
+        public async Task<IActionResult> GetContacts()
         {
-            return Ok(dbContext.Contacts.ToList());
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        //Add a new contact
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                FullName = addContactRequest.FullName,
+                Email = addContactRequest.Email,
+                Phone = addContactRequest.Phone,
+                Address = addContactRequest.Address,
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(contact);
         }
     }
 }
