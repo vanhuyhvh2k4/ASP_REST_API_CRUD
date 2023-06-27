4a3fe79815725da315cf59a814aa24ef9007ec2d
anhhuydepzai <anhhuydepzai@gmail.com>
2023-06-27 13:45:09 +0700

    use entity framework and connect to mysql

--- ASP_REST_API_CRUD/Program.cs
+++ ASP_REST_API_CRUD/Program.cs
@@ -9,8 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
-
+//builder.Services.AddDbContext<ContactsAPIDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
+builder.Services.AddDbContext<ContactsAPIDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnectionString"),
+        new MySqlServerVersion(new Version(8, 0, 26))));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
